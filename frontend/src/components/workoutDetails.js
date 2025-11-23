c0551ce85f3f52daede440f1be5eb86c46820213
Thongek Next Wongpomon <thongek_wpm@hotmail.com>
2025-11-23 20:53:33 +0700

    Update workoutDetails.js

--- frontend/src/components/workoutDetails.js
+++ frontend/src/components/workoutDetails.js
@@ -1,5 +1,19 @@
-const WorkoutDetails = () => {
-  return <div>Workout Details Component</div>;
+const WorkoutDetails = ({ workout }) => {
+  return (
+    <div className="workout-details">
+      <h2>{workout.title}</h2>
+      <p>
+        <strong>Load (kg):</strong> {workout.load}
+      </p>
+      <p>
+        <strong>Reps:</strong> {workout.reps}
+      </p>
+      <p>
+        <strong>Created At:</strong>{" "}
+        {new Date(workout.createdAt).toLocaleString()}
+      </p>
+    </div>
+  );
 };
 
 export default WorkoutDetails;
