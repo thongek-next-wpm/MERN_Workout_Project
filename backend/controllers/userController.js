288a5332d773309fff7508cbab0ce887cba06af7
Thongaek Wongpomon <thongek_wpm@hotmail.com>
2025-11-26 12:12:42 +0700

    commit Sign Up and Hashing Password

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -8,7 +8,13 @@ const signinUser = (req, res) => {
 
 // signup user controller
 const signupUser = (req, res) => {
-  res.json({ message: "signup" });
+  const { email, password } = req.body;
+  try {
+    const user = User.signup(email, password);
+    res.status(200).json(email, password, user);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 module.exports = { signinUser, signupUser };
