c41fda4b14d4301d73c10904452722f202aee9d6
Thongek Next Wongpomon <thongek_wpm@hotmail.com>
2025-12-03 00:06:47 +0700

    commit Making a useSignup Hook

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -1,16 +1,22 @@
 import { useState } from "react";
+import { useSignup } from "../hooks/useSignup";
 
 const Signup = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const handleSubmit = (e) => {
+  const { signup, error, isLoading } = useSignup();
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(email, password);
+
+    await signup(email, password);
   };
+
   return (
     <form className="signup" onSubmit={handleSubmit}>
       <h3>Sign Up</h3>
-      <label>Email:</label>
+
+      <label>Email address:</label>
       <input
         type="email"
         onChange={(e) => setEmail(e.target.value)}
@@ -22,7 +28,9 @@ const Signup = () => {
         onChange={(e) => setPassword(e.target.value)}
         value={password}
       />
-      <button type="submit">Sign Up</button>
+
+      <button disabled={isLoading}>Sign up</button>
+      {error && <div className="error">{error}</div>}
     </form>
   );
 };
