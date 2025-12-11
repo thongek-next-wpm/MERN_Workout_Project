7186699b034cdae3bc7dbe81d225becb41869707
Thongek Next Wongpomon <thongek_wpm@hotmail.com>
2025-12-11 22:00:36 +0700

    Commit Protecting Rweact Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useAuthContext } from "./hooks/useAuthContext";
 // Pages & Components
 import Home from "./pages/Home";
 import Login from "./pages/Login";
@@ -6,19 +7,29 @@ import Signup from "./pages/Signup";
 import Navbar from "./components/Navbar";
 
 function App() {
+  const { user } = useAuthContext();
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar />
         <div className="pages">
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route
+              path="/"
+              element={user ? <Home /> : <Navigate to="/login" />}
+            />
           </Routes>
           <Routes>
-            <Route path="/login" element={<Login />} />
+            <Route
+              path="/login"
+              element={!user ? <Login /> : <Navigate to="/" />}
+            />
           </Routes>
           <Routes>
-            <Route path="/signup" element={<Signup />} />
+            <Route
+              path="/signup"
+              element={!user ? <Signup /> : <Navigate to="/" />}
+            />
           </Routes>
         </div>
       </BrowserRouter>
